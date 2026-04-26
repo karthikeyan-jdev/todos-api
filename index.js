94ea16b46202aa1ecdd3f30bd74701711d6d3a4d
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2026-04-26 19:31:50 +0530

    Build check 01

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import todoRoutes from "./routes/todoRoutes.js";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-
-dotenv.config();
 
 // Connect DB
 await connectDB();
@@ -13,9 +10,9 @@ await connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
