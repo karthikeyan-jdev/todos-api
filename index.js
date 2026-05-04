f3487c50f2b65bf87f6f18b0954747d16dafa94d
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2026-05-04 16:11:02 +0530

    Added login API

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import todoRoutes from "./routes/todoRoutes.js";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
-// Connect DB
 await connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,7 +20,6 @@ app.get("/", (req, res) => {
 app.use("/api/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
 
-// ✅ ONLY run locally
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
