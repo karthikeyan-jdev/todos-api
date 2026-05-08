5fe505067cb4bff8be44cb805ee337588bf19889
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2026-05-08 17:19:29 +0530

    cors bug fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:4173", "https://todos-web-eta.vercel.app"],
+    origin: ["http://localhost:5173", "https://todos-web-eta.vercel.app"],
     credentials: true,
   }),
 );
